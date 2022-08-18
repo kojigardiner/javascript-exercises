2ce71092d2e2d0e69cd13b3fbc47e2107287f274
Koji Gardiner <koji.gardiner@gmail.com>
2022-08-18 13:22:48 -0700

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lower = string.toLowerCase();
+    const chars = Array.from(lower).filter(char => isAlphaNumeric(char));  // remove all non-alphanumeric characters
 
+    for (let i = 0; i < Math.floor(chars.length / 2); i++) {
+        if (chars[i] !== chars[chars.length - 1 - i]) return false;
+    }
+    return true;
 };
+
+function isAlphaNumeric(char) {
+    const code = char.charCodeAt(0);
+    return ((code > 47 && code < 58) ||     // 0-9
+        (code > 64 && code < 91) ||         // A-Z
+        (code > 96 && code < 123));         // a-z
+}
 
 // Do not edit below this line
 module.exports = palindromes;
