ac773c03bca38d559d657a4963e3ede23df4cbdc
Koji Gardiner <koji.gardiner@gmail.com>
2022-08-17 22:43:57 -0700

    01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, count) {
+    if (count < 0) return "ERROR";
 
+    returnString = "";
+    for (let i = 0; i < count; i++) {
+        returnString += string;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
