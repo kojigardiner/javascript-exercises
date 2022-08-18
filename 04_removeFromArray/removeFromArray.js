29df93e316e96d807b002d536b21a89b2b83c103
Koji Gardiner <koji.gardiner@gmail.com>
2022-08-17 22:56:19 -0700

    Completed 03 and 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        to_remove = args[i];
 
+        array = array.filter(item => item !== to_remove);
+    }
+    return array;
 };
 
 // Do not edit below this line
