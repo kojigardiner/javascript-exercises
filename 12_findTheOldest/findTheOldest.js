e0f9cb263dad0ff718ac3a030285f8c2e66d3d59
Koji Gardiner <koji.gardiner@gmail.com>
2022-08-18 13:39:32 -0700

    Completed 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const sorted = people.sort((a, b) => {
+        if (!a.yearOfDeath) aAge = (new Date()).getFullYear() - a.yearOfBirth;
+        else aAge = a.yearOfDeath - a.yearOfBirth;
+        if (!b.yearOfDeath) bAge = (new Date()).getFullYear() - b.yearOfBirth;
+        else bAge = b.yearOfDeath - b.yearOfBirth;
 
-};
+        return aAge < bAge ? 1 : -1;
+    });
+    return sorted[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
