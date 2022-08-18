a352f26279575a44439b833a862704126d97e04f
Koji Gardiner <koji.gardiner@gmail.com>
2022-08-17 23:05:05 -0700

    Completed 05 and 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (typeof num1 != 'number' || typeof num2 != 'number') return "ERROR";
 
+    start = Math.min(num1, num2);
+    end = Math.max(num1, num2);
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
