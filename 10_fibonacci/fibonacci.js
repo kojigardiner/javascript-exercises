a0928e1e44a28566d345316bebcc1e54733c3b14
Koji Gardiner <koji.gardiner@gmail.com>
2022-08-18 13:29:41 -0700

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
+    if (a < 1) return "OOPS";
+    let idx = +a;
 
+    if (idx == 1) return 1;
+    if (idx == 2) return 1;
+
+    return fibonacci(idx - 1) + fibonacci(idx - 2);
 };
 
 // Do not edit below this line
